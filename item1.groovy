fcf45bc24f7d2850cea26d4177b070b15895bac3
clin@bsch.bancroftschool.org <clin@bsch.bancroftschool.org@MDCLAB-88-009-26>
2024-02-28 11:45:47 -0500

    created large champhered cube

--- item1.groovy
+++ item1.groovy
@@ -1,9 +1,17 @@
 // code here
 import eu.mihosoft.vrl.v3d.ChamferedCube
 
-new ChamferedCube(
+
+CSG cube = new ChamferedCube(
 	200,// X 
 	100, // Y
 	50, // Z
 	15 // Chamfer length
 	).toCSG()
+
+CSG cubeSmall = cube
+		.scalex(3)
+		.scaley(3)
+		.scalez(3)
+
+return cubeSmall
